3be2a58c2148cd755fc9e33bde92bab0532fef8e
Belhenniche Yassaa <Yagon66@gmail.com>
2019-04-06 18:47:31 +0100

    feat: add Book component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
-import { Link, Route } from "react-router-dom";
+import * as BooksAPI from './BooksAPI'
+import { Link, Route } from 'react-router-dom';
 import './App.css'
+import Book from './components/Book';
 
-class BooksApp extends React.Component {
+class BooksApp extends React.PureComponent {
   state = {
+    books: []
+  }
+
+  componentDidMount() {
+    this.fetchBooks();
+  }
+
+  fetchBooks = () => {
+    BooksAPI.getAll().then((books) => {
+      // console.log(books);
+      this.setState({ books: books });
+    });
+  }
+
+  updateShelf = (book, shelf) =>  {
+    BooksAPI.update(book, shelf).then(() => {
+      this.fetchBooks();
+    });
   }
 
   render() {
@@ -15,7 +34,6 @@ class BooksApp extends React.Component {
           <div className="logo">
             <h1>B</h1>
           </div>
-
           <div className="menu">
             <h3>BROWSE</h3>
             <ul>
@@ -33,47 +51,53 @@ class BooksApp extends React.Component {
             <React.Fragment>
               <h2>All Books</h2>
               <div className="bookShelf">
-
-                <div className="book">
-
-                  <div className="book-top">
-                    <div className="book-cover" style={{ backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
-                    <div className="book-shelf-changer">
-                      <select>
-                        <option value="move" disabled>Move to...</option>
-                        <option value="currentlyReading">Currently Reading</option>
-                        <option value="wantToRead">Want to Read</option>
-                        <option value="read">Read</option>
-                        <option value="none">None</option>
-                      </select>
-                    </div>
-                  </div>
-                  <div className="book-title">To Kill a Mockingbird</div>
-                  <div className="book-authors">Harper Lee</div>
-                </div>
-
+                {
+                  this.state.books
+                    .map((book, index) => (<Book book={book} key={index} onUpdateShelf={this.updateShelf} />))
+                }
               </div>
             </React.Fragment>
           )} />
-
           <Route path="/reading" render={() => (
-            <h2>Currently Reading</h2>
-            // <BookList books={} />
+            <React.Fragment>
+              <h2>Currently Reading</h2>
+              <div className="bookShelf">
+                {
+                  this.state.books
+                    .filter(book => book.shelf === 'currentlyReading')
+                    .map((book, index) => (<Book book={book} key={index} onUpdateShelf={this.updateShelf} />))
+                }
+              </div>
+            </React.Fragment>
           )} />
-
           <Route path="/toread" render={() => (
-            <h2>Want to Read</h2>
-            // <BookList books={} />
+            <React.Fragment>
+              <h2>Want to Read</h2>
+              <div className="bookShelf">
+                {
+                  this.state.books
+                    .filter(book => book.shelf === 'wantToRead')
+                    .map((book, index) => (<Book book={book} key={index} onUpdateShelf={this.updateShelf} />))
+                }
+              </div>
+            </React.Fragment>
           )} />
-
           <Route path="/read" render={() => (
-            <h2>Read</h2>
-            // <BookList books={} />
+            <React.Fragment>
+              <h2>Read</h2>
+              <div className="bookShelf">
+                {
+                  this.state.books
+                    .filter(book => book.shelf === 'read')
+                    .map((book, index) => (<Book book={book} key={index} onUpdateShelf={this.updateShelf} />))
+                }
+              </div>
+            </React.Fragment>
           )} />
-
           <Route path="/search" render={() => (
-            <h2>Search</h2>
-            // <BookList books={} />
+            <React.Fragment>
+              <h2>Search</h2>
+            </React.Fragment>
           )} />
         </div>
 
---
+++ src/components/Book.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Book = (props) => (
+    <div className="book">
+        <div className="book-top">
+            <div className="book-cover" style={{ backgroundImage: `url(${props.book.imageLinks.thumbnail})` }}></div>
+            <div className="book-shelf-changer">
+                <select
+                    onChange={e => {
+                        props.onUpdateShelf(props.book, e.target.value)
+                    }}
+                    value={props.book.shelf}>
+                    <option value="move" disabled>Move to...</option>
+                    <option value="currentlyReading">Currently Reading</option>
+                    <option value="wantToRead">Want to Read</option>
+                    <option value="read">Read</option>
+                    <option value="none">None</option>
+                </select>
+            </div>
+        </div>
+        <div className="book-title">{props.book.title}</div>
+        <div className="book-authors">{props.book.authors}</div>
+    </div>
+)
+
+export default Book;
