b70ae37b4dc2d83af40d9dda1694def013e2a0db
Belhenniche Yassaa <Yagon66@gmail.com>
2019-04-08 01:00:02 +0100

    refactor: change Links to Navlinks for styling

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const SideMenu = () => (
     <div className="side-content">
@@ -9,11 +9,11 @@ const SideMenu = () => (
     <div className="menu">
       <h3>BROWSE</h3>
       <ul>
-        <li><Link to="/">All Books</Link></li>
-        <li><Link to="/reading">Currently Reading</Link></li>
-        <li><Link to="/toread">Want to Read</Link></li>
-        <li><Link to="/read">Read</Link></li>
-        <li><Link to="/search">Search</Link></li>
+        <li><NavLink to="/" exact activeClassName="current" >All Books</NavLink></li>
+        <li><NavLink to="/reading" activeClassName="current" >Currently Reading</NavLink></li>
+        <li><NavLink to="/toread" activeClassName="current" >Want to Read</NavLink></li>
+        <li><NavLink to="/read" activeClassName="current" >Read</NavLink></li>
+        <li><NavLink to="/search" activeClassName="current" >Search</NavLink></li>
       </ul>
     </div>
   </div>
