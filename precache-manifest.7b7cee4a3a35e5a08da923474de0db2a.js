50cf1a678ebe6f8963f3b117396a8502ce815860
Belhenniche Yassaa <Yagon66@gmail.com>
2019-06-05 15:16:40 +0100

    Updates

---
+++ precache-manifest.7b7cee4a3a35e5a08da923474de0db2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3944ebdb28b2a27d24453f8d727bc5b",
+    "url": "/MyReads-Udacity/static/media/bookPage.b3944ebd.jpg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/MyReads-Udacity/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "d182a2eb969140ef4d6c",
+    "url": "/MyReads-Udacity/static/js/runtime~main.d182a2eb.js"
+  },
+  {
+    "revision": "0c86b8bc71286310dc40",
+    "url": "/MyReads-Udacity/static/js/main.0c86b8bc.chunk.js"
+  },
+  {
+    "revision": "7d2f23ddbec356443373",
+    "url": "/MyReads-Udacity/static/js/1.7d2f23dd.chunk.js"
+  },
+  {
+    "revision": "0c86b8bc71286310dc40",
+    "url": "/MyReads-Udacity/static/css/main.7331ce32.chunk.css"
+  },
+  {
+    "revision": "1afa2f8eea807a746c232cf8d08b848e",
+    "url": "/MyReads-Udacity/index.html"
+  }
+];
